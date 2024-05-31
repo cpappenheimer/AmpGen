1335d82e2a932667e49aa54c66e05a238dfe073d
Cheryl Pappenheimer <cheryl.theroux@gmail.com>
2024-05-31 10:38:22 -0400

    Fix

--- src/ConcurrentUtilities.cpp
+++ src/ConcurrentUtilities.cpp
@@ -4,13 +4,17 @@ namespace AmpGen
 {
     NamedParameter<size_t> getNumThreads()
     {
-        static const unsigned int THREAD_MOD = 8;
-
+        static const unsigned int THREAD_MOD = 10;
         unsigned int maxThreads = std::thread::hardware_concurrency();
+        unsigned int threadsToUse = maxThreads / THREAD_MOD;
+        if (threadsToUse <= 0)
+        {
+            threadsToUse = 1;
+        }
 
         NamedParameter<size_t> nThreads = NamedParameter<size_t>(
-            "nCores",
-            maxThreads / THREAD_MOD,
+            "nThreads",
+            threadsToUse,
             "Number of threads to use");
         INFO("Using " << nThreads.getVal() << " / " << maxThreads << " threads (DECREASE IN ConcurrentUtilities.cpp IF FREEZING)");
 
@@ -20,12 +24,17 @@ namespace AmpGen
 #ifdef _OPENMP
     NamedParameter<unsigned int> getNCores()
     {
-        static const unsigned int CORE_MOD = 8;
-
+        static const unsigned int CORE_MOD = 10;
         unsigned int concurrentThreadsSupported = std::thread::hardware_concurrency();
+        unsigned int threadsToUse = concurrentThreadsSupported / CORE_MOD;
+        if (threadsToUse <= 0)
+        {
+            threadsToUse = 1;
+        }
+
         NamedParameter<unsigned int> nCores = NamedParameter<unsigned int>(
             "nCores",
-            concurrentThreadsSupported / CORE_MOD,
+            threadsToUse,
             "Number of cores to use (OpenMP only)");
 
         INFO("Using OpenMP " << nCores.getVal() << " / " << concurrentThreadsSupported << " threads (DECREASE IN ConcurrentUtilities.cpp IF FREEZING)");
